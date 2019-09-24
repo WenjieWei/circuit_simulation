fcd06f11c863f7c4d4c145750076f75ea171e53d
Wenjie Wei <wenjieweizb@gmail.com>
2019-09-24 16:32:01 -0400

    logic finished. Debug required. TODO: in nlJacobian.m, modify the logic so that the code inspects if the cathod of the diode is grounded.

--- assignments/src/a2/dcsolve.m
+++ assignments/src/a2/dcsolve.m
@@ -9,6 +9,62 @@ function [Xdc dX] = dcsolve(Xguess,maxerr)
 %         newton Iteration. the size of dX should be the same as the number
 %         of Newton-Raphson iterations. See the help on the function 'norm'
 %         in matlab.
-global G C b DIODE_LIST
+    global G C b DIODE_LIST
+    
+    % Vector contains the result for each iteration. Overwritten for each iteration. 
+    Xdc = zeros(size(G, 2), 1); 
+    
+    % Delta X vector for each iteration. Size will be increased for each iteration. 
+    dX = zeros(size(G, 2), 1); 
+    
+    % temporary vector containing the results of Is(exp(Vs/Vt) - 1)
+    f = zeros(size(G, 2), 1);
+    
+    diodeStartNode = DIODE_LIST.node1;
+    diodeEndNode = DIODE_LIST.node2;
+    Is = DIODE_LIST.Is;
+    Vt = DIODE_LIST.Vt;
+    
+    converged = false;
+    iteration = 0;
+    
+    while ~converged
+        iteration = iteration + 1;
+        
+        % Calculate the Phi vector for each iteration. 
+        if diodeStartNode ~= 0 && diodeEndNode ~= 0
+            % If the diode is not connected to the ground. 
+            f(diodeStartNode) = Is * (exp((Xguess(diodeStartNode) - Xguess(diodeEndNode)) / Vt) - 1);
+            f(diodeEndNode) = -Is * (exp((Xguess(diodeStartNode) - Xguess(diodeEndNode)) / Vt) - 1);
+        else
+            % If the cathod of the diode is connected to the ground
+            f(diodeStartNode) = Is * (exp(Xguess(diodeStartNode) / Vt) - 1);
+        end
+        
+        % Phi vector containing the temporary result to obtain dX.
+        Phi = G * Xguess + f - b;
+        
+        % Calculate the Jacobian. 
+        J = nlJacobian(Xguess);
+        
+        dX = [dX (-inv(J) * Phi)];
+        
+        % Determine the converge condition. 
+        % Check if every entry of current iteration meets the threashold.
+        % If there is at least one dX > maxerr, 
+        % Continue the iteration. 
+        if all(dX < maxerr)
+            converged = true;
+        end
+    end
+end
+
+
+
+
+
+
+
+
 
 
--- assignments/src/a2/nlJacobian.m
+++ assignments/src/a2/nlJacobian.m
@@ -1,9 +1,39 @@
 function J = nlJacobian(X)
-% Compute the jacobian of the nonlinear vector of the MNA equations as a 
-% function of X
-% input: X is the current value of the unknown vector.
-% output: J is the jacobian of the nonlinear vector f(X) in the MNA
-% equations. The size of J should be the same as the size of G.
+    % Compute the jacobian of the nonlinear vector of the MNA equations as a 
+    % function of X
+    % input: X is the current value of the unknown vector.
+    % output: J is the jacobian of the nonlinear vector f(X) in the MNA
+    % equations. The size of J should be the same as the size of G.
+    
+    % Diode curve: I = Is(exp(V/VT) - 1)
+    global G DIODE_LIST
+    
+    startNode = DIODE_LIST.node1;
+    endNode = DIODE_LIST.node2;
+    Is = DIODE_LIST.Is;
+    Vt = DIODE_LIST.Vt;
+    
+    v1 = X(startNode);
+    v2 = X(endNode);
+    
+    F = zeros(size(G, 1), size(G, 2));
+    
+    F(startNode, startNode) = (Is / Vt) * exp((v1 - v2) / Vt);
+    F(startNode, endNode) = -(Is / Vt) * exp((v1 - v2) / Vt);
+    F(endNode, startNode) = -(Is / Vt) * exp((v1 - v2) / Vt);
+    F(endNode, endNode) = (Is / Vt) * exp((v1 - v2) / Vt);
+    
+    J = G - F;
+end
 
-global G DIODE_LIST
+
+
+
+
+
+
+
+
+
+
 
